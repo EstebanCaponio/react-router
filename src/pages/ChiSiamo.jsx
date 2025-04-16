efc77c84924e1125067cea61ed7b95e2c0af6b6e
EstebanCaponio <estebancaponio@gmail.com>
2025-04-16 15:16:40 +0200

    feat: bonus 2/3

--- src/pages/ChiSiamo.jsx
+++ src/pages/ChiSiamo.jsx
@@ -1,4 +1,4 @@
-import NavBar from "../components/NavBar";
+
 
 export default function ChiSiamo() {
     return (
--- src/pages/ListaDeiPost.jsx
+++ src/pages/ListaDeiPost.jsx
@@ -1,9 +1,27 @@
-import NavBar from "../components/NavBar";
+import axios from "axios";
+import { useState } from "react"
 
 export default function ListaDeiPost() {
+
+    const [posts, setPosts] = useState([]);
+
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(res => {
+            setPosts(res.data)
+        })
+
     return (
         <>
             <h1>LISTA PRODOTTI</h1>
+
+            <div>
+                {posts.map(post =>
+                    <div key={post.id}>
+                        <h3>{post.title}</h3>
+                        <p>{post.body}</p>
+                        <hr />
+                    </div>)}
+            </div>
         </>
     )
 }
